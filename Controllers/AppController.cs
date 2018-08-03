306772ee1e3e5436b95e494cec4ed71d9b4d335d
sumit <sumitsharnov@gmail.com>
2018-08-03 10:38:50 +0530

    Stage 1

---
+++ Controllers/AppController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact US";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About US";
+            return View();
+        }
+
+    }
+}
---
+++ wwwroot/index.js
@@ -0,0 +1,5 @@
+﻿var button = document.getElementById("button");
+button.addEventListener("click", function () {
+    alert("Are you sure!!!!");
+
+})
